2c35a8cc7aae424f1ad0c6880adb6e90ce308ee8
Kumar Saurav <diesel.saurav@gmail.com>
2012-09-08 22:28:00 +0530

    removed unused files

--- routes/snippet.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app, ensureAuthenticated) {
-	app.get('/snippet/new', ensureAuthenticated, function (req, res) {
-    	res.render('snippet/new', { user: req.user });
-	});
-
-	app.post('/snippet', ensureAuthenticated, function (res, res) {
-
-	});
-}
